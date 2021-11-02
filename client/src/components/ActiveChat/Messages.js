308a8efc0c6bc337f648c7bc41f82cd58ebb546e
jimmdd <jimdingmac@gmail.com>
2021-11-02 11:09:38 -0400

    fix message display and order issue for ticket #1

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,6 +5,8 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  // sort message in created time asc order to display messages correctly
+  messages.sort((a, b) => moment(a.createdAt).isBefore(b.createdAt) ? -1 : 1)
 
   return (
     <Box>

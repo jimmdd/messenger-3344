259077cd2dd01df476ca716fc68d8d752b9a8ecf
jimmdd <jimdingmac@gmail.com>
2021-11-13 17:47:46 -0500

    Adding many to many relation to Conversation and User table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,12 +3,10 @@ const User = require("./user");
 const Message = require("./message");
 
 // associations
-
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+Conversation.belongsToMany(User, { through: 'User_Conversations' });
+User.belongsToMany(Conversation, { through: 'User_Conversations' });
 
 module.exports = {
   User,

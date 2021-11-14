8797416ad6874c5bdf0ff120d1a8edef97d3d3dc
jimmdd <jimdingmac@gmail.com>
2021-11-14 00:51:21 -0500

    add message status table to support multi read status in group chats

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,15 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const MessageStatus = require("./messageStatus");
 
 // associations
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
-Conversation.belongsToMany(User, { through: 'User_Conversations' });
-User.belongsToMany(Conversation, { through: 'User_Conversations' });
+Conversation.belongsToMany(User, { through: 'user_conversation' });
+User.belongsToMany(Conversation, { through: 'user_conversation' });
+Message.belongsToMany(Conversation, { through: MessageStatus })
+Conversation.belongsToMany(Message, { through: MessageStatus })
 
 module.exports = {
   User,
